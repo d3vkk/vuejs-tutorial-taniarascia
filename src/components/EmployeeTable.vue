af18e2dab28f7ce3f381e420e2a72c64bdb4a869
d3vkk <donaldkk22@yahoo.com>
2020-06-29 15:12:18 +0300

    initial commit

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="employee-table">
+    <table>
+      <!-- ...thead... -->
+      <tbody>
+        <th>Employee name</th>
+        <th>Employee email</th>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+    export default {
+    name: 'employee-table',
+    props: {
+        employees: Array,
+    },
+    }
+</script>
+
+<style scoped></style>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 v-if="" @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
